584bbc10eb56ee9ea4e1d4afeb216516635be0d5
codercgx <529488618@qq.com>
2022-06-07 16:25:45 +0800

    项目初始化搭建

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const Koa = require('koa')
+const app = require('./app')
 
-const app = new Koa()
+const config = require('./app/config')
 
-app.listen(8000, () => {
-  console.log('服务器启动成功')
+app.listen(config.APP_PORT, () => {
+  console.log('服务器在${config.APP_PORT}端口启动成功~')
 })
